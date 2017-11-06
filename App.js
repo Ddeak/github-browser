06d048c5c488b1548c06e1e0bc6bc83147f344b3
Alistair Nelson <alistair@alt-nelson.com>
2017-11-06 20:39:36 +0000

    More code tidying, moving the navigation code out to its own file.
    This is to ensure any changes are restricted to a single file where possible.

--- App.js
+++ App.js
@@ -1,48 +1,8 @@
 import React, { Component } from 'react';
-import { Alert, Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
-import { NavigationActions, StackNavigator, TabNavigator } from 'react-navigation'
-import base64 from 'base-64'
-import Profile from './screens/Profile/Profile'
-import Organisations from './screens/Organisations/Organisations'
-import Repositories from './screens/Repositories/Repositories'
-import Login from './screens/Login/Login'
+import GithubBrowserApp from './navigation'
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-const TabbedScreens = TabNavigator({
-  Profile: { screen: Profile },
-  Organisations: { screen: Organisations },
-  Repositories: { screen: Repositories }
-})
-
-const SecuredScreens = StackNavigator({
-  TabbedScreens: {
-    screen: TabbedScreens
-  }
-})
-
-const UnsecuredScreens = StackNavigator({
-  Login: { screen: Login }
-})
-
-const SimpleApp = StackNavigator({
-  Login: { screen: UnsecuredScreens },
-  Profile: { screen: SecuredScreens }
-}, {
-  headerMode: 'none'
-})
-
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return <SimpleApp />;
+    return <GithubBrowserApp />;
   }
 }
---
+++ navigation/index.js
@@ -0,0 +1,31 @@
+import { NavigationActions, StackNavigator, TabNavigator } from 'react-navigation'
+
+import Profile from '../screens/Profile/Profile'
+import Organisations from '../screens/Organisations/Organisations'
+import Repositories from '../screens/Repositories/Repositories'
+import Login from '../screens/Login/Login'
+
+const TabbedScreens = TabNavigator({
+  Profile: { screen: Profile },
+  Organisations: { screen: Organisations },
+  Repositories: { screen: Repositories }
+})
+
+const SecuredScreens = StackNavigator({
+  TabbedScreens: {
+    screen: TabbedScreens
+  }
+})
+
+const UnsecuredScreens = StackNavigator({
+  Login: { screen: Login }
+})
+
+const GithubBrowserApp = StackNavigator({
+  Login: { screen: UnsecuredScreens },
+  Profile: { screen: SecuredScreens }
+}, {
+  headerMode: 'none'
+})
+
+export default GithubBrowserApp
